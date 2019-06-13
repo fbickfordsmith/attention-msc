02c50f6fe900c48560c940cf2eba2f40af4278c0
Freddie <fbickfordsmith@gmail.com>
2019-06-13 15:18:35 +0100

    Improved modularity

--- splitting_data.py
+++ splitting_data.py
@@ -5,29 +5,36 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 '''
 Take a CSV where each row is a set of ImageNet class names (eg, ['n01440764', ...]).
 For each set, i,
-    Move the folders for the in-set classes into a new folder called `seti`.
-    In the `seti` folder, create empty folders for all out-of-set classes.
+- Move the folders for the in-set classes into a new folder called `seti`.
+- In the `seti` folder, create empty folders for all out-of-set classes.
+
+Arguments:
+folder in {'train', 'val', 'val_white'}
 
 References:
 thispointer.com/how-to-create-a-directory-in-python/
 thispointer.com/python-how-to-move-files-and-directories/
+
+Copying a folder using bash:
+old_folder contains a, b, c
+No slash:
+    cp -r .../old_folder .../new_folder
+    new_folder contains old_folder
+With slash:
+    cp -r .../old_folder/ .../new_folder
+    new_folder contains a, b, c
+cp -r /mnt/fast-data16/datasets/ILSVRC/2012/clsloc/ /home/freddie/ILSVRC2012
 '''
 
-# old_folder contains a, b, c
-# cp -r ~/.../old_folder ~/.../new_folder => new_folder contains old_folder
-# cp -r ~/.../old_folder/ ~/.../new_folder => new_folder contains a, b, c
-
-# cp -r ~/Downloads/a ~/Downloads/d
-# cp -r /mnt/fast-data16/datasets/ILSVRC/2012/clsloc/ /home/freddie/ILSVRC2012
-# cp -r /mnt/fast-data16/datasets/ILSVRC/2012/clsloc/ /mnt/fast-data16/datasets/freddie
-
+import sys
 import shutil
 import numpy as np
 
-class_sets = np.loadtxt('vgg16/class_set_names.csv', dtype=str, delimiter=',')
+folder = sys.argv[1] # sys.argv[0] is the name of the script
+class_sets = np.loadtxt('class_sets.csv', dtype=str, delimiter=',')
+path_to_data = '/home/freddie/ILSVRC2012/clsloc/'+folder+'/'
 # path_to_data = 'ILSVRC2012/ILSVRC2012_img_val_copy/'
-# path_to_data = '/mnt/fast-data16/datasets/ILSVRC/2012/clsloc/train/'
-path_to_data = '/home/freddie/ILSVRC2012/clsloc/val/'
+print(f'Running {sys.argv[0]} on {path_to_data}')
 
 for i, inset_classes in enumerate(class_sets):
     set_folder = f'set{i:02}/'

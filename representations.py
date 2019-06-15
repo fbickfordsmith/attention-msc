02b77fcdc653fb9355397103018a0a9ae7fdc168
Freddie <fbickfordsmith@gmail.com>
2019-06-15 09:14:50 +0100

    Added representations.py

--- representations.py
+++ representations.py
@@ -13,6 +13,7 @@ from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.layers import Input
+from sklearn.metrics.pairwise import cosine_similarity
 
 pretrained_model = VGG16(weights='imagenet')
 model_in = Input(batch_shape=(None, 224, 224, 3))
@@ -22,7 +23,7 @@ for layer in pretrained_model.layers[2:22]:
 modified_model = Model(model_in, model_out)
 
 path_to_data = '/mnt/fast-data16/datasets/ILSVRC/2012/clsloc/train/'
-path_to_save = '/home/freddie/vgg16/activations/'
+path_to_activations = '/home/freddie/activations/'
 batch_size = 256
 datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 generator = datagen.flow_from_directory(
@@ -38,8 +39,14 @@ activations = modified_model.predict_generator(
     workers=7,
     verbose=True)
 
+mean_activations = []
 for i in range(generator.num_classes):
-    np.save(
-        path_to_save+f'class{i:04}_activations',
-        activations[np.flatnonzero(generator.classes==i)],
-        allow_pickle=False)
+    all_activations = activations[np.flatnonzero(generator.classes==i)]
+    mean_activation = np.mean(all_activations, axis=0)
+    mean_activations.append(mean_activation)
+    path_to_save = path_to_activations + f'class{i:04}'
+    np.save(path_to_save+'_all_activations', all_activations, allow_pickle=False)
+    np.save(path_to_save+'_mean_activation', mean_activation, allow_pickle=False)
+mean_activations = np.array(mean_activations)
+similarity = cosine_similarity(mean_activations)
+np.save(path_to_activations+'cosine_similarity', similarity, allow_pickle=False)

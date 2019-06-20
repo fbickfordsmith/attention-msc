4be994008cf694610402c77d680f53e9414cb988
Freddie <fbickfordsmith@gmail.com>
2019-06-20 18:36:05 +0100

    Updated image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -8,21 +8,40 @@ References:
 import cv2
 from keras.applications.vgg16 import preprocess_input
 
+# def robinson_processing(img):
+#     # img is RGB
+#     # load as BGR and convert to RGB: img = cv2.imread(path_img)[:, :, ::-1]
+#
+#     # resize
+#     height = img.shape[0] * 256//min(img.shape[:2])
+#     width = img.shape[1] * 256//min(img.shape[:2])
+#     img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+#
+#     # crop
+#     start_row = height//2 - 224//2
+#     start_col = width//2 - 224//2
+#     end_row = start_row + 224
+#     end_col = start_col + 224
+#     img = img[start_row:end_row, start_col:end_col]
+#
+#     # preprocess_input converts to BGR
+#     return preprocess_input(img)
+
 def robinson_processing(img):
-    # img is RGB
-    # load as BGR and convert to RGB: img = cv2.imread(path_img)[:, :, ::-1]
+    # Load as BGR and convert to RGB
+#     img = cv2.imread(path_img)[:, :, ::-1]
 
-    # resize
-    height = img.shape[0] * 256//min(img.shape[:2])
-    width = img.shape[1] * 256//min(img.shape[:2])
-    img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+    # Resize
+    height, width, _ = img.shape
+    new_height = height * 256//min(img.shape[:2])
+    new_width = width * 256//min(img.shape[:2])
+    img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
 
-    # crop
-    start_row = height//2 - 224//2
-    start_col = width//2 - 224//2
-    end_row = start_row + 224
-    end_col = start_col + 224
-    img = img[start_row:end_row, start_col:end_col]
+    # Crop
+    height, width, _ = img.shape
+    start_x = width//2 - 224//2
+    start_y = height//2 - 224//2
+    img = img[start_y:(start_y+224), start_x:(start_x+224)]
 
-    # preprocess_input converts to BGR
+    # Gets converted to BGR
     return preprocess_input(img)

8de8e16279e335df24fe3a72a56a6a828c69d73e
Freddie <fbickfordsmith@gmail.com>
2019-06-23 17:53:10 +0100

    Updated img_processing_trial.py

--- img_processing_trial.py
+++ img_processing_trial.py
@@ -2,6 +2,7 @@ import os
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
+import sys
 import numpy as np
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing.image import ImageDataGenerator
@@ -9,18 +10,22 @@ from image_processing import robinson_processing
 
 model = VGG16(weights='imagenet')
 model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['acc'])
-path = '/mnt/fast-data16/datasets/ILSVRC/2012/clsloc/val_white/' # path to examples (should be in category folders)
+path = '/mnt/fast-data16/datasets/ILSVRC/2012/clsloc/'
+partition = sys.argv[1] # one of [train, val, val_white]
 batch_size = 256
 datagen = ImageDataGenerator(preprocessing_function=robinson_processing)
-generator_eval = datagen.flow_from_directory(
-    directory=path,
+
+generator = datagen.flow_from_directory(
+    directory=path+partition,
     target_size=(224, 224),
     batch_size=batch_size,
     shuffle=False,
     class_mode='categorical')
+
 scores = model.evaluate_generator(
-    generator_eval,
-    steps=int(np.ceil(generator_eval.n/generator_eval.batch_size)),
+    generator,
+    steps=int(np.ceil(generator.n/generator.batch_size)),
     use_multiprocessing=False,
     verbose=True)
-print(f'Using evaluate_generator, {model.metrics_names} = {scores}')
+
+print(f'{model.metrics_names} = {scores}')

ad5d88eb504c2c068de301ac61d285a26278b7de
Freddie <fbickfordsmith@gmail.com>
2019-08-01 12:25:51 +0100

    Updated contexts_testing_df.py

--- contexts_testing_df.py
+++ contexts_testing_df.py
@@ -28,33 +28,19 @@ path_data = f'/mnt/fast-data16/datasets/ILSVRC/2012/clsloc/{data_partition}/'
 path_dataframes = f'/home/freddie/dataframes_{data_partition}/{type_context}contexts/'
 path_results = '/home/freddie/attention/results/'
 num_contexts = len(os.listdir(path_dataframes))
-scores_incontext, scores_outofcontext = [], []
+scores_ic, scores_ooc = [], []
 
 for i in range(int(context_start), int(context_end)):
     name_context = f'{type_context}context{i:02}'
     print(f'\nTesting on {name_context}')
     W = np.load(f'{path_weights}{name_context}_weights.npy')
     model = build_model(Lambda(lambda x: W * x), train=False)
-
-    # evaluate on in-context data
     dataframe_ic = pd.read_csv(f'{path_dataframes}{name_context}_df.csv')
-    scores_ic = np.array(evaluate_model(model, dataframe_ic, path_data))
-    scores_incontext.append(scores_ic)
+    dataframe_ooc = pd.read_csv(f'{path_dataframes}{name_context}_df_out.csv')
+    scores_ic.append(evaluate_model(model, dataframe_ic, path_data))
+    scores_ooc.append(evaluate_model(model, dataframe_ooc, path_data))
 
-    # evaluate on out-of-context data
-    scores_ooc = []
-    for j in range(num_contexts):
-        if j != i:
-            name_ooc = f'{type_context}context{j:02}'
-            dataframe_ooc = pd.read_csv(f'{path_dataframes}{name_ooc}_df.csv')
-            scores_ooc.append(evaluate_model(model, dataframe_ooc, path_data))
-    scores_ooc = np.mean(np.array(scores_ooc), axis=0)
-    scores_outofcontext.append(scores_ooc)
-
-scores_arr = np.concatenate((
-    np.array(scores_incontext),
-    np.array(scores_outofcontext)),
-    axis=1)
+scores_arr = np.concatenate((np.array(scores_ic), np.array(scores_ooc)), axis=1)
 
 # list(itertools.chain(list1, list2, ...)) returns a flattened list
 col_names = list(itertools.chain(

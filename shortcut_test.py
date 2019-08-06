cf7c39fe81d98d5dd4521095b3d6b6cb815e2918
Freddie <fbickfordsmith@gmail.com>
2019-08-06 12:18:56 +0100

    Fixed bugs

--- shortcut_test.py
+++ shortcut_test.py
@@ -3,11 +3,28 @@ os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
 from models import build_model
+from keras.applications.vgg16 import preprocess_input
 from models_vgg2 import build_vgg2
 import time
 
 data_partition = 'train'
 path_data = f'/mnt/fast-data16/datasets/ILSVRC/2012/clsloc/{data_partition}/'
+path_activations = '/home/freddie/activations-conv/'
+datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+generator0 = datagen.flow_from_directory(
+    directory=path_data,
+    class_mode=None, # None => returns just images (no labels)
+    target_size=(224, 224),
+    batch_size=256,
+    shuffle=True)
+
+generator1 = datagen.flow_from_directory(
+    directory=path_activations,
+    class_mode=None, # None => returns just images (no labels)
+    target_size=(224, 224),
+    batch_size=256,
+    shuffle=True)
 
 time0 = time.time()
 model0 = build_model()
@@ -17,16 +34,9 @@ time0 = time.time()
 model1 = build_vgg2()
 print(f'New model: build time = {time.time()-time0} seconds')
 
-generator = datagen.flow_from_directory(
-    directory=path_data,
-    class_mode=None, # None => returns just images (no labels)
-    target_size=(224, 224),
-    batch_size=256,
-    shuffle=False)
-
 time0 = time.time()
 predictions0 = model0.predict_generator(
-    generator=generator,
+    generator=generator0,
     steps=steps(generator.n, generator.batch_size),
     use_multiprocessing=True,
     verbose=True)
